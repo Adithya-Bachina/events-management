4e7bba3dd69b44fa13d37c92dc7808b3e7774a56
Adithya-Bachina <adithyabachina@gmail.com>
2024-10-21 22:13:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './home';
 import AboutUs from './about';
 import RegistrationForm from './register.jsx';
